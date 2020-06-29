978f10ec43edfa3de5cca41239752fcaa2448e44
aaltmark <adamaltmark@gmail.com>
2020-06-29 13:49:06 -0500

    Adam 1

---
+++ app/controllers/bloggers_controller.rb
@@ -0,0 +1,22 @@
+class BloggersController < ApplicationController
+    def index 
+        @bloggers = Blogger.all
+    end
+
+    def show 
+        @blogger = Blogger.find(params[:id])
+    end 
+
+    def new
+        @blogger = Blogger.new
+    end 
+
+    def create
+        blogger = Blogger.create(blogger_params)
+        redirect_to blogger_path(blogger)
+    end
+
+    
+
+
+end
